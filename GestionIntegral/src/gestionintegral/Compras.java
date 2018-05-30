c7606a31d490bcd38d523d3e2541a7a402973a92
informatica4 <informatica4@RafaelGomez>
2018-05-30 11:54:44 +0200

    Compras 2.1 (Solucionado bug 001)

--- GestionIntegral/src/gestionintegral/Compras.java
+++ GestionIntegral/src/gestionintegral/Compras.java
@@ -2,6 +2,6 @@ package gestionintegral;
 
 public class Compras {
 	public static void main (String[] args){
-		System.out.println("Compras 2.1");
+		System.out.println("Compras 2.1 (Solucionado bug 001)");
 	}
 }
